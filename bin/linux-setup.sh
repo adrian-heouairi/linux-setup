dff51e8974b6a519387913f8aeb4ba47247c9aef
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-10-30 21:25:07 +0100

    Added support for keeping the default shortcut when adding a custom shortcut

--- bin/linux-setup.sh
+++ bin/linux-setup.sh
@@ -110,25 +110,33 @@ fi
 kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta ""
 qdbus org.kde.KWin /KWin reconfigure
 
+# Third argument must be only one shortcut, fourth argument must be the default shortcuts in KDE separated by \\t (\\t between simple quotes in Bash) for this shortcut or nothing
 put_kde_keyboard_shortcut() {
-    sed -i "/$3\(,\|\\t\)/d" ~/.config/kglobalshortcutsrc
+    sed -i "/$3\(,\|\\\\t\)/d" ~/.config/kglobalshortcutsrc # $3 may contain '+' so we can't use sed -E
     kwriteconfig5 --file kglobalshortcutsrc --group "$1" --key "$2" "$3",,
+    
+    # \t or \\t doesn't work with kwriteconfig5
+    if [ "$4" ]; then
+        sed -i "s/^\($2=$3\),,$/\1\\\\t$4,$4,/" ~/.config/kglobalshortcutsrc
+    fi
 }
 
 # See ~/.config/kglobalshortcutsrc
 killall kglobalaccel5
-put_kde_keyboard_shortcut kmix mic_mute Alt+Z
-put_kde_keyboard_shortcut kmix mute Alt+X
+put_kde_keyboard_shortcut kmix mic_mute Alt+Z 'Microphone Mute'
+put_kde_keyboard_shortcut kmix mute Alt+X 'Volume Mute'
 put_kde_keyboard_shortcut plasmashell 'toggle do not disturb' Alt+C
-put_kde_keyboard_shortcut mediacontrol playpausemedia Meta+Z
-put_kde_keyboard_shortcut kmix decrease_volume Meta+A
-put_kde_keyboard_shortcut kmix increase_volume Meta+S
-put_kde_keyboard_shortcut plasmashell 'activate task manager entry 4' Meta+Q
-put_kde_keyboard_shortcut plasmashell 'activate task manager entry 5' Meta+W
-put_kde_keyboard_shortcut plasmashell 'activate task manager entry 6' Meta+E
-put_kde_keyboard_shortcut plasmashell 'activate task manager entry 7' Meta+Shift+Q
-put_kde_keyboard_shortcut plasmashell 'activate task manager entry 8' Meta+Shift+W
-put_kde_keyboard_shortcut plasmashell 'activate task manager entry 9' Meta+Shift+E
+put_kde_keyboard_shortcut mediacontrol playpausemedia Meta+Z 'Media Play'
+put_kde_keyboard_shortcut kmix decrease_volume Meta+A 'Volume Down'
+put_kde_keyboard_shortcut kmix increase_volume Meta+S 'Volume Up'
+put_kde_keyboard_shortcut plasmashell 'activate task manager entry 4' Meta+Q Meta+4
+put_kde_keyboard_shortcut plasmashell 'activate task manager entry 5' Meta+W Meta+5
+put_kde_keyboard_shortcut plasmashell 'activate task manager entry 6' Meta+E Meta+6
+put_kde_keyboard_shortcut plasmashell 'activate task manager entry 7' Meta+Shift+Q Meta+7
+put_kde_keyboard_shortcut plasmashell 'activate task manager entry 8' Meta+Shift+W Meta+8
+put_kde_keyboard_shortcut plasmashell 'activate task manager entry 9' Meta+Shift+E Meta+9
+put_kde_keyboard_shortcut kwin 'Window Close' Meta+X Alt+F4
+put_kde_keyboard_shortcut org.kde.krunner.desktop _launch Alt+Space Alt+F2
 kglobalaccel5 & disown
 
 #crontab -l > /tmp/linux-setup-crontab 2>/dev/null
