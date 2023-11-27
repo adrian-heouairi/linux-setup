36c0bc8988a2706785b8f32853374c3dc79dfe01
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-11-28 00:04:46 +0100

    Added comment

--- setup-scripts/once-recommended.d/kde-copy-config-files.sh
+++ setup-scripts/once-recommended.d/kde-copy-config-files.sh
@@ -2,4 +2,6 @@
 
 source "${0%/*}"/../../resources/setup-scripts-base.sh
 
+# Use linux-setup-backup-kde-conf.sh to generate the source
+
 rsync -rl -- "$(linux-setup-get-resources-path.sh)"/kde/my-kde-conf/ ~/
