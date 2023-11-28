4aad2762631dc6a8a84ac736d4f853a26e45127b
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-11-28 10:12:59 +0100

    Added Konsole custom profile to KDE conf

---
+++ resources/kde/my-kubuntu-22.04-kde-conf/.local/share/konsole/Profile 1.profile
@@ -0,0 +1,11 @@
+[Appearance]
+ColorScheme=BlackOnWhite
+Font=Hack,16,-1,5,50,0,0,0,0,0
+
+[General]
+LocalTabTitleFormat=%w
+Name=Profile 1
+Parent=FALLBACK/
+
+[Scrolling]
+HistoryMode=2
--- setup-scripts/once-recommended.d/kde-copy-config-files.sh
+++ setup-scripts/once-recommended.d/kde-copy-config-files.sh
@@ -2,6 +2,6 @@
 
 source "${0%/*}"/../../resources/setup-scripts-base.sh
 
-# Use linux-setup-backup-kde-conf.sh to generate the source
+# Use linux-setup-backup-kde-conf.sh my-conf to generate the conf backup
 
-rsync -rl -- "$(linux-setup-get-resources-path.sh)"/kde/my-kde-conf/ ~/
+rsync -rl -- "$(linux-setup-get-resources-path.sh)"/kde/my-kubuntu-22.04-kde-conf/ ~/
