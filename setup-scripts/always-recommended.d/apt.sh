bbc19ccfa87c9a844df645bd05c93bed092b9388
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-12-03 09:37:33 +0100

    Moved Thunar bulk renamer .desktop file

--- setup-scripts/always-recommended.d/apt.sh
+++ setup-scripts/always-recommended.d/apt.sh
@@ -14,3 +14,5 @@ echo 'pidof -x syncthing || exec syncthing --no-browser' > ~/.config/linux-setup
 echo 'pidof -x fcitx5 || exec fcitx5' > ~/.config/linux-setup/autostart/fcitx5.sh
 
 cp -f -- "$(linux-setup-get-resources-path.sh)/dot-desktop-files/qpaeq.desktop" ~/.local/share/applications/
+
+cp -f -- "$(linux-setup-get-resources-path.sh)/dot-desktop-files/thunar-bulk-renamer.desktop" ~/.local/share/applications/
