26559ad87c21d4349430307d13c95b2fb1005d10
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-12-04 12:00:28 +0100

    Improved AutoKey Super+C

--- resources/autokey/data/CUSTOM/CtrlC with conversion to file URLs.py
+++ resources/autokey/data/CUSTOM/CtrlC with conversion to file URLs.py
@@ -2,9 +2,13 @@ import os; exec(open(os.getenv('HOME') + '/bin/autokey-ctrl-c.py').read())
 
 system.exec_command('''echo a
 
-c=$(cat '{}')
-printf %s "$c" | xsel -b
-notify-send -- 'Copied file URL(s) to clipboard' "$c"
+c=$(cat -- '{}')
+if [ "$c" ]; then
+    printf %s "$c" | xsel -b
+    notify-send -i clipboard -- 'URL copy' "Copied file URL(s) to clipboard:\n$c"
+else
+    notify-send -i clipboard -- 'URL copy' "There were no paths in clipboard, restored previous clipboard"
+fi
 
 '''.format(CTRL_C_file_urls_file))
 
