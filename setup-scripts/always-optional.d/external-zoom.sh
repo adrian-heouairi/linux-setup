7dba23e5f093309d43b15c3d8dc0053fae781078
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-12-04 23:31:27 +0100

    Fixed Zoom download URL

--- setup-scripts/always-optional.d/external-zoom.sh
+++ setup-scripts/always-optional.d/external-zoom.sh
@@ -2,4 +2,4 @@
 
 source "${0%/*}"/../../resources/setup-scripts-base.sh
 
-linux-setup-install-deb.sh zoom 'https://zoom.us/client/5.16.10.668/zoom_amd64.deb'
+linux-setup-install-deb.sh zoom 'https://zoom.us/client/latest/zoom_amd64.deb'
