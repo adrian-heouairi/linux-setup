4d5911729ebbf2567f5e90a69ef4705cebe1f5bb
Adrian Heouairi <adrian.heouairi@gmail.com>
2024-01-07 13:51:47 +0100

    Update

---
+++ bin/minted.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+path=$1
+
+fullpath=$(realpath -- "$path")
+
+if ! [ -e "$fullpath" ]; then
+    notify-send ".tex file doesn't exist"
+    exit 1
+fi
+
+dir=$(dirname -- "$fullpath")
+file=$(basename -- "$fullpath")
+
+[ -e "$dir"/main.tex ] && file=main.tex
+
+cd -- "$dir" || exit 1
+
+pdflatex -halt-on-error -shell-escape "$dir/$file"
+pdflatex -halt-on-error -shell-escape "$dir/$file"
--- bin/mpv-open.sh
+++ bin/mpv-open.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$HOSTNAME" = aetu2 ] && ! mountpoint /mnt/Shared-SMB-atnr; then
+    notify-send "Mount SMB first"
+    exit 1
+fi
+
 mpv-watch-later-save-restore.sh restore
 mpv-command-lines-save-restore.sh restore
 
