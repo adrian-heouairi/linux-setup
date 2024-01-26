cae58aa4488f4400163da01f5207e4b54cb74484
Adrian Heouairi <adrian.heouairi@gmail.com>
2024-01-26 14:57:14 +0100

    Update

--- bin/alsftp-setup.sh
+++ bin/alsftp-setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 {
-sudo sed -Ezi 's/# acls-sftp start.*# acls-sftp end\n?//' /etc/fstab
+sudo sed -Ezi 's/#alsftp-start.*#alsftp-end\n?//' /etc/fstab
 
-# See sftp-server.conf example in resources
-conf=$(grep -v '^#' ~/.config/linux-setup/sftp-server.conf) || exit 1
+# See alsftp.conf example in resources
+conf=$(grep -v '^#' ~/.config/linux-setup/alsftp.conf) || exit 1
 
 local_ip_last_byte=$(get-local-ip-last-byte.sh) || exit 1
 
 port="$local_ip_last_byte"022
-group=acls-sftp
-roots_dir=/ACLS/sftp-roots
+group=alsftp
+roots_dir=/als/alsftp-chroots
 
 message_at_end=
 fstab_lines=
@@ -19,18 +19,18 @@ sudo apt install -y openssh-server curl
 
 sudo groupadd -- "$group"
 
-users_to_remove=$(cut -d: -f1 /etc/passwd | grep '^acls-sftp-')
+users_to_remove=$(cut -d: -f1 /etc/passwd | grep '^alsftp-')
 IFS=$'\n'
 for i in $users_to_remove; do
     sudo userdel -- "$i"
 done
 
-public_ip=$(curl https://ipinfo.io/IP 2>/dev/null)
+public_ip=$(curl https://ipinfo.io/IP)
 
 IFS=$'\n'
 for line in $conf; do
     dir_to_share=$(cut -d$'\t' -f1 <<< "$line")
-    username=acls-sftp-$(cut -d$'\t' -f2 <<< "$line")
+    username=alsftp-$(cut -d$'\t' -f2 <<< "$line")
     password=$(cut -d$'\t' -f3 <<< "$line")
     permissions=$(cut -d$'\t' -f4 <<< "$line")
 
@@ -45,6 +45,8 @@ for line in $conf; do
     }
 
     if [ "$permissions" != none ]; then
+        sudo chown -R "$UID:$UID" -- "$dir_to_share"
+
         sudo setfacl -R -d -m o::"$permissions" -- "$dir_to_share"
         sudo setfacl -R -m o::"$permissions" -- "$dir_to_share"
     fi
@@ -55,25 +57,23 @@ for line in $conf; do
     #     find "$dir_to_share" '(' '!' -type l ')' -exec sudo chmod o=rwx {} ';'
     # fi
 
-    share_basename=$(basename -- "$dir_to_share")
-
-    sudo useradd -g "$group" -d /"$share_basename" -s /sbin/nologin -- "$username"
+    sudo useradd -g "$group" -d /shared -s /sbin/nologin -- "$username"
     yes -- "$password" | sudo passwd -- "$username"
 
-    bind_mount_dir=$roots_dir/$username/$share_basename
+    bind_mount_dir=$roots_dir/$username/shared
 
     sudo mkdir -p -- "$bind_mount_dir"
     sudo mount --bind -- "$dir_to_share" "$bind_mount_dir"
     fstab_lines+="$dir_to_share $bind_mount_dir none bind,nofail 0 0"$'\n'
 
-    message_at_end+="Connect with command 'sftp sftp://$username@$public_ip:$port' or mount with command 'sshfs $username@$public_ip: <mount-dir> -p $port -o reconnect' with password $password"$'\n'
+    message_at_end+="Connect with command 'sftp sftp://$username@$public_ip:$port' or mount with command 'sshfs $username@$public_ip:/shared <mount-dir> -p $port -o reconnect' with password $password"$'\n'
 done
 
 if [ "$fstab_lines" ]; then
     echo Adding to fstab:
-    echo '# acls-sftp start' | sudo tee -a /etc/fstab
+    echo '#alsftp-start' | sudo tee -a /etc/fstab
     printf %s "$fstab_lines" | sudo tee -a /etc/fstab
-    echo '# acls-sftp end' | sudo tee -a /etc/fstab
+    echo '#alsftp-end' | sudo tee -a /etc/fstab
 fi
 
 echo -n \
@@ -86,11 +86,11 @@ AllowGroups $group
 Match Group $group
 ChrootDirectory $roots_dir/%u
 ForceCommand internal-sftp
-" | sudo tee /etc/ssh/sshd_config.d/CUSTOM-sftp.conf > /dev/null
+" | sudo tee /etc/ssh/sshd_config.d/CUSTOM-alsftp.conf > /dev/null
 
 sudo systemctl enable ssh.service
 sudo systemctl restart ssh.service
 
 echo ========================================
 printf %s "$message_at_end"
-} 2>&1 | tee ~/.config/linux-setup/sftp-server-output.txt
+} 2>&1 | tee ~/.config/linux-setup/alsftp-setup-output.txt
